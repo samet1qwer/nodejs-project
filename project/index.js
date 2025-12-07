7ae46d998f380c7407570e1843889cf482a43ac0
CyberTempest1w <samet102a@gmail.com>
2025-12-07 22:44:05 +0300

    template edit and settings db

---
+++ project/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+const path = require("path");
+// mongoose
+mongoose
+  .connect("mongodb://127.0.0.1:27017/portfolio")
+  .then(() => {
+    console.log("Mongoose is connected ✅");
+  })
+  .catch((err) => console.log("mongoose connection error:", err));
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+app.set("view engine", "ejs");
+
+app.set("views", "./views");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/user")));
+app.use(express.static(path.join(__dirname, "public/admin")));
+
+const AboutMe = require("./models/aboutMe");
+
+const save = async () => {
+  try {
+    const result = await about.save();
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
+save();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// routers
+const userRouter = require("./routers/user/user");
+const adminRouter = require("./routers/admin/auth");
+const { type } = require("os");
+
+app.use(userRouter);
+app.use(adminRouter);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ project/routers/admin/auth.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const about = require("../../models/aboutMe");
+
+router.get("/admin", (req, res) => {
+  res.render("admin/sign-in");
+});
+
+router.post("/admin", (req, res) => {
+  const data = req.body;
+  console.log("lorem", data.email, data.password);
+
+  about
+    .find({ email: data.email, password: data.password })
+    .then((result) => {
+      if (result.length > 0) {
+        res.render("admin/home");
+      } else {
+        res.render("admin/sign-in", {
+          message: "Invalid email or password",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
