8fb96a445d5952d0aae41c9c67b7f0eeaac4ea40
CyberTempest1w <samet102a@gmail.com>
2025-12-17 19:17:20 +0300

    get information data

--- project/routers/user/user.js
+++ project/routers/user/user.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const about = require("../../models/About");
+const information = require("../../models/information");
 
 router.get("/", async (req, res) => {
   const aboutData = await about.findById("693ee6cf762ca52bf1e17250");
-  res.render("user/home", { session: req.session, about: aboutData });
+  const informationData = await information.findById(
+    "693f1d2587d877cfa6184790"
+  );
+  res.render("user/home", {
+    session: req.session,
+    about: aboutData,
+    information: informationData,
+  });
 });
 
 module.exports = router;
