d8a39ced0c0c47c7f5656a8c74f10734f7842872
CyberTempest1w <samet102a@gmail.com>
2025-12-17 19:09:25 +0300

    get about data home page

--- project/routers/user/user.js
+++ project/routers/user/user.js
@@ -4,7 +4,7 @@ const about = require("../../models/About");
 
 router.get("/", async (req, res) => {
   const aboutData = await about.findById("693ee6cf762ca52bf1e17250");
-  res.render("user/home", { session: req.session, aboutData: aboutData });
+  res.render("user/home", { session: req.session, about: aboutData });
 });
 
 module.exports = router;
